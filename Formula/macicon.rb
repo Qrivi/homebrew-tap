e3f44f58611108e1e7450f5454257530f8a1ba43
Kristof Dewilde <code@qrivi.dev>
2024-03-15 07:42:57 +0100

    Bump macicon to 1.0.1

--- Formula/macicon.rb
+++ Formula/macicon.rb
@@ -2,14 +2,14 @@ class Macicon < Formula
     desc "Fast macOS-styled app icon generator"
     homepage "https://github.com/Qrivi/macicon"
     license "MIT"
-    version "1.0.0"
+    version "1.0.1"
 
     if Hardware::CPU.intel?
-        url "https://github.com/Qrivi/macicon/releases/download/1.0.0/macicon-x86_64.tar.gz"
-        sha256 "2de926a7b16c7187964bdc6b4cd8e53ea676c8f3deff259fcefa57e2302035ba"
+        url "https://github.com/Qrivi/macicon/releases/download/1.0.1/macicon-x86_64.tar.gz"
+        sha256 "b85c3ed74a27c78c981a730fa5e4575a5b22aa68e198fc72bbc859e5a00c6d11"
     else
-        url "https://github.com/Qrivi/macicon/releases/download/1.0.0/macicon-arm64.tar.gz"
-        sha256 "018e1a3d11327c2453e55050a6a56eca68f0946bf4e359d74ccae7d81a305fee"
+        url "https://github.com/Qrivi/macicon/releases/download/1.0.1/macicon-arm64.tar.gz"
+        sha256 "71bbfa4640c2b4bdf775b875726e7d9802563aae6dca3c854f368b66c612e491"
     end
 
     depends_on :macos
